83646d6ac574ba6218bb57ccd129da68b765d15e
alexander.iii@mail.ru <alexander.iii@mail.ru>
2025-02-25 22:52:27 +0500

    Fixed .gitignore

--- pay/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import create_checkout_session, item_detail
-
-urlpatterns = [
-    path('buy/<int:id>/', create_checkout_session, name="buy"),
-    path('item/<int:id>/', item_detail, name="item_detail"),
-]
